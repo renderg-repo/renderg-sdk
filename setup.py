c42d5fee389011b91d7b736c2502cb5276139935
zhangyongcun <zhangpaisen@gmail.com>
2023-06-19 11:38:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='renderg-sdk',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/renderg-repo/renderg-sdk.git',
     author='RenderG',
     author_email='support@renderg.com',
