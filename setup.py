a6530d900ffb71e070d884d69390d6fae71cf191
wangxin <1643265797@qq.com>
2023-06-20 15:37:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
+from os import path
+
 from setuptools import setup, find_packages
+
+
+def get_readme():
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
 
 setup(
     name='renderg-sdk',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/renderg-repo/renderg-sdk.git',
     author='RenderG',
     author_email='support@renderg.com',
     description='www.renderg.com',
+    long_description=get_readme(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),    
     install_requires=[
         'requests',
@@ -15,5 +27,10 @@ setup(
     package_data={
         'analyze_houdini': ["./houdini/*"],
         'ascp': ["./*/*"],
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
 )
